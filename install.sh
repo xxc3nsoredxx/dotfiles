3ae562f911d720892d7e850f3a82bef95f483dc5
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2020-12-16 20:07:46 -0600

    Redid the installer mechanics
    Manually loop instead of using the `select` builtin.
    Installer is mostly untested.
    Therefore does everythin up to, but not including, the actual
    installation.

--- install.sh
+++ install.sh
@@ -16,45 +16,111 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+RUNDIR=$PWD
 CONTENTS=($(find home -depth -type f -exec realpath --relative-base=home \{\} \+))
 declare -a NOT_INSTALLED
 
-# Respect custom PS3 prompts
-if [ -e $PS3 ]; then
-    PS3="Install: "
-fi
+# Installs the given file (using hardlinks)
+function install_file {
+    printf  "%-035s <<< %s\n" "$HOME/$i" "$RUNDIR/$i"
+    return
+    DIR_PART=$(dirname $1)
+    pushd $HOME >/dev/null
+        # Create required directories if needed
+        mkdir -p $DIR_PART
+        cd $DIR_PART
+        ln $RUNDIR/home/$1
+    popd >/dev/null
+}
 
 # Find the files which are not installed
 for i in ${CONTENTS[@]}; do
     if [[ ! -a $HOME/$i ]]; then
         NOT_INSTALLED[${#NOT_INSTALLED[@]}]=$i
+    elif [[ ! -z $(diff -q $HOME/$i home/$i) ]]; then
+        echo "Contents of \"$HOME/$i\" differ from the repo"
     fi
 done
 
+# Installer menu
+echo "Separate choices by space (' ') to install multiple files at once."
+echo "NOTE: 'all' and 'exit' are mutually exclusive with any other choices."
+echo "      Individual choices take precedence over 'all' and 'exit'."
 while [[ -z $QUIT ]]; do
-    select FILE in ${NOT_INSTALLED[@]} all exit; do
-        # Turn into a proper array index
-        REPLY=$(($REPLY - 1))
+    # Output choices
+    COUNT=1
+    for NAME in ${NOT_INSTALLED[@]} all exit; do
+        echo "${COUNT}) $NAME"
+        COUNT=$(($COUNT + 1))
+    done
 
-        # Have to index into the indices of the array
-        INDEX=(${!NOT_INSTALLED[@]})
+    # Prompt for input
+    echo -n "Install: "
+    readarray -n 1 INPUT
+
+    # Sort and remove duplicate entries
+    INPUT=($(echo "${INPUT[@]}" | tr -s ' ' '\n' | sort | uniq))
+
+    # These values depend on the state before any files are installed
+    # Number of not installed files
+    N_NOT_INSTALLED=${#NOT_INSTALLED[@]}
+    # The array can (and probably will) have holes, need to index using the
+    # array of valid indices
+    INDICES=(${!NOT_INSTALLED[@]})
+
+    # Parse each choice from the input
+    for C in ${INPUT[@]}; do
+        # Remove non-numeric characters
+        CHOICE=${C//[^0-9]}
+
+        # Non-numeric choice
+        if [ "$CHOICE" != "$C" ]; then
+            echo "Invalid choice: '$C' ..."
+            continue
+        # Out of bounds choices
+        elif [ $CHOICE -eq 0 ]; then
+            echo "Invalid choice: '$CHOICE' ..."
+            continue
+        elif [ $CHOICE -gt $(($N_NOT_INSTALLED + 2)) ]; then
+            echo "Invalid choice: '$CHOICE' ..."
+            continue
+        fi
+
+        # Turn into a proper array index
+        CHOICE=$(($CHOICE - 1))
 
         # Test if "all" was selected
-        if [[ $REPLY -eq ${#NOT_INSTALLED[@]} ]]; then
-            echo "Install all"
-            QUIT=1
+        if [[ $CHOICE -eq $N_NOT_INSTALLED ]]; then
+            # Test if "all" is the only choice
+            if [ ${#INPUT[@]} -eq 1 ]; then
+                echo "Installing all ..."
+                for f in ${NOT_INSTALLED[@]}; do
+                    install_file $f
+                done
+                unset NOT_INSTALLED
+            else
+                echo "Invalid choice in this context: 'all' ..."
+            fi
         # Test if "exit" was selected
-        elif [[ $REPLY -eq $((${#NOT_INSTALLED[@]} + 1)) ]]; then
-            echo "Exit"
-            exit
-        # Install the selected file
-        elif [[ $REPLY -lt ${#NOT_INSTALLED[@]} ]]; then
-            echo $FILE
-            unset NOT_INSTALLED[${INDEX[$REPLY]}]
-        # Invalid
+        elif [[ $CHOICE -eq $(($N_NOT_INSTALLED + 1)) ]]; then
+            # Test if "exit" is the only choice
+            if [ ${#INPUT[@]} -eq 1 ]; then
+                echo "Exit..."
+                QUIT=1
+            else
+                echo "Invalid choice in this context: 'exit' ..."
+            fi
+        # Install
         else
-            echo "Invalid choice!"
+            install_file $FILE
+            unset NOT_INSTALLED[${INDICES[$CHOICE]}]
         fi
-        break
     done
+
+    # Check if no more files left
+    if [[ ${#NOT_INSTALLED[@]} -eq 0 ]]; then
+        echo "Nothing left to install, exit..."
+        QUIT=1
+    fi
+    exit
 done
